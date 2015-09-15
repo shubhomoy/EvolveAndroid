4d865af59a29f3b012b19755fd16f12190abf211
SHUBHOMOY BISWAS <biswas_shubhomoy777@yahoo.com>
2015-09-15 10:06:35 +0530

    initial commit

---
+++ app/src/main/java/com/evolve/evolve/EvolveActivities/MainActivity.java
@@ -0,0 +1,17 @@
+package com.evolve.evolve.EvolveActivities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.evolve.evolve.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
