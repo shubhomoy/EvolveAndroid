444ce1bdde90540abbde7e8483328b9a7f806b38
Shubhakar <shubh.cool94@gmail.com>
2015-09-15 23:46:13 +0530

    initial cmmit

---
+++ app/src/main/java/adapters/MainPageAdapter.java
@@ -0,0 +1,29 @@
+package adapters;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import java.util.ArrayList;
+
+/**
+ * Created by ddvlslyr on 15/9/15.
+ */
+public class MainPageAdapter extends FragmentStatePagerAdapter {
+
+    ArrayList<Fragment> list;
+    public MainPageAdapter(FragmentManager fm,ArrayList<Fragment> list) {
+        super(fm);
+        this.list=list;
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        return list.get(i);
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+}
--- app/src/main/java/com/evolve/evolve/EvolveActivities/MainActivity.java
+++ app/src/main/java/com/evolve/evolve/EvolveActivities/MainActivity.java
@@ -1,17 +1,108 @@
 package com.evolve.evolve.EvolveActivities;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import com.evolve.evolve.R;
+import com.getbase.floatingactionbutton.FloatingActionButton;
+import com.getbase.floatingactionbutton.FloatingActionsMenu;
+
+import java.util.ArrayList;
+
+import adapters.MainPageAdapter;
+import fragments.GalleryFragment;
+import fragments.QuickListFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private ViewPager pager;
+    private FloatingActionButton fab1,fab2;
+    private FloatingActionsMenu fabMenu;
+    private ArrayList<Fragment> list;
+    private MainPageAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        instantiate();
+
+        adapter=new MainPageAdapter(getSupportFragmentManager(),list);
+        pager.setAdapter(adapter);
+
+        fab1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "SortByName", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        fab2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"SortByAddress",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        pager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                switch (position){
+                    case 0:
+                        fabMenu.setVisibility(View.VISIBLE);
+                        break;
+                    case 1:
+                        fabMenu.setVisibility(View.GONE);
+                        break;
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+    }
+
+    private void instantiate(){
+        toolbar= (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        pager= (ViewPager) findViewById(R.id.view_pager);
+        list=new ArrayList<>();
+        list.add(new GalleryFragment());
+        list.add(new QuickListFragment());
+
+        fabMenu= (FloatingActionsMenu) findViewById(R.id.multiple_actions);
+        fab1= (FloatingActionButton) findViewById(R.id.action_a);
+        fab2= (FloatingActionButton) findViewById(R.id.action_b);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId()==R.id.search)
+            Toast.makeText(this,"Yet to come",Toast.LENGTH_SHORT).show();
+        return super.onOptionsItemSelected(item);
     }
 }
