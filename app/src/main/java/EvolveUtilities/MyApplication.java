176dcafbc2b7d8aae5ba45ab04fd39b616f07b7e
SHUBHOMOY BISWAS <biswas_shubhomoy777@yahoo.com>
2015-09-18 23:22:40 +0530

    volley added

---
+++ app/src/main/java/EvolveUtilities/MyApplication.java
@@ -0,0 +1,25 @@
+package EvolveUtilities;
+
+import android.app.Application;
+import android.content.Context;
+
+/**
+ * Created by shubhomoy on 18/9/15.
+ */
+public class MyApplication extends Application {
+    private static MyApplication instance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance = this;
+    }
+
+    public static MyApplication getInstance() {
+        return instance;
+    }
+
+    public static Context getAppContext() {
+        return instance.getApplicationContext();
+    }
+}
