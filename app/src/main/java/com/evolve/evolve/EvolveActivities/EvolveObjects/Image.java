9ab400dd7c0e1ae87e9cf3d16635b93bd9bacd38
Agam Gupta <agamgupta2895@gmail.com>
2015-09-19 17:10:53 +0530

    share location feature added

---
+++ app/src/main/java/com/evolve/evolve/EvolveActivities/EvolveObjects/Image.java
@@ -0,0 +1,13 @@
+package com.evolve.evolve.EvolveActivities.EvolveObjects;
+
+/**
+ * Created by vellapanti on 19/9/15.
+ */
+public class Image {
+    String name;
+    String description;
+    String date;
+    String latitude;
+    String logitude;
+    String slug;
+}
--- app/src/main/java/com/evolve/evolve/EvolveActivities/PreviewActivity.java
+++ app/src/main/java/com/evolve/evolve/EvolveActivities/PreviewActivity.java
@@ -1,16 +1,23 @@
 package com.evolve.evolve.EvolveActivities;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.location.Location;
+import android.location.LocationManager;
 import android.media.ExifInterface;
 import android.os.Environment;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 
 import com.evolve.evolve.EvolveActivities.EvolveUtilities.ImageManipulator;
@@ -27,15 +34,43 @@ public class PreviewActivity extends AppCompatActivity {
     private ImageView previewImageView;
     private String file_name;
     private Intent mainIntent;
-
+    private double latitude;
+    private double longitude;
+    private boolean isGPSEnabled;
+    private boolean isNetworkEnabled;
+    protected LocationManager locationManager;
+    public Location location;
+    CheckBox checkBox;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preview);
         instantiate();
+
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean choice) {
+                if(choice){
+                    isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+                    isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+                    if(isGPSEnabled && isNetworkEnabled) {
+                        location=locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                        latitude = location.getLatitude();
+                        longitude = location.getLongitude();
+                        Log.d("gps",String.valueOf(latitude)+ " " + String.valueOf(longitude));
+                    }
+                    else{
+                        showSettingsAlert();
+                        checkBox.setChecked(false);
+                    }
+                }
+            }
+        });
         file_name=mainIntent.getStringExtra("file");
         Bitmap bitmap= BitmapFactory.decodeFile(Environment.getExternalStorageDirectory().toString()+"/Evolve/temp/img_"+file_name+".jpg");
         previewImageView.setImageBitmap(bitmap);
+
+
     }
 
 
@@ -44,6 +79,8 @@ public class PreviewActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         previewImageView=(ImageView)findViewById(R.id.image_temp);
         mainIntent = getIntent();
+        checkBox=(CheckBox)findViewById(R.id.loc);
+        locationManager=(LocationManager) this.getSystemService(LOCATION_SERVICE);
     }
 
     @Override
@@ -78,4 +115,28 @@ public class PreviewActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void showSettingsAlert(){
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(PreviewActivity.this);
+        alertDialog.setTitle("GPS is settings");
+        alertDialog.setMessage("GPS is not enabled. Do you want to go to settings menu?");
+
+        alertDialog.setPositiveButton("Settings", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            }
+        });
+
+        alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        alertDialog.create().show();
+    }
+
+
+
 }
